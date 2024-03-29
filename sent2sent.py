c6f1495f605f77d494e900e9e249564efe77ba1d
Robert Edwardes <rdedwardes@gmail.com>
2024-03-29 19:32:56 -0400

    spelling

--- sent2sent.py
+++ sent2sent.py
@@ -1,6 +1,6 @@
 import torch
 def sent2sent (sentence1, sentence2):
-    predic_out= {0:'contradiction', 1:'netural', 2:'entailment'}
+    predic_out= {0:'contradiction', 1:'neutral', 2:'entailment'}
     roberta = torch.hub.load('pytorch/fairseq', 'roberta.large.mnli')
     roberta.eval()
     with torch.no_grad():
